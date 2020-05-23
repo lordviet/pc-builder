4ebb7d8b0f197b20c705eee6c2a6c1a90a0d9404
lordviet <viet1231@abv.bg>
2020-05-23 05:06:34 +0300

    Add abstract class component

---
+++ PCB/AppClasses/Component.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCB.AppClasses
+{
+    abstract class Component
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public static string connectionString = ConfigurationManager
+            .ConnectionStrings["connString"]
+            .ConnectionString;
+
+        public DataTable Select(string tableName)
+        {
+            // Db Connection 
+            var connection = new SqlConnection(connectionString);
+            var dataTable = new DataTable();
+
+            try
+            {
+                // SQL Query
+                var query = $"SELECT * FROM {tableName}";
+                var cmd = new SqlCommand(query, connection);
+
+                // Creating SQL Data Adapter using cmd
+                var adapter = new SqlDataAdapter(cmd);
+                connection.Open();
+
+                // Filling the data table
+                adapter.Fill(dataTable);
+            }
+            catch (Exception e)
+            {
+                // Error handling
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dataTable;
+        }
+    }
+}
